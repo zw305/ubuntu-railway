e9aa866887ec1c59492025e2f5b7488403f1a204
zw305 <zw11305@outlook.com>
2026-07-18 21:09:46 +0800

    Update websockify to use port 5901

--- start-vnc.sh
+++ start-vnc.sh
@@ -4,7 +4,7 @@
 vncserver -geometry 1280x720 -depth 24 -localhost no :1
 
 # 启动 noVNC
-websockify --web=/usr/share/novnc 6080 localhost:5900 &
+websockify --web=/usr/share/novnc 6080 localhost:5901 &
 
 # 保持容器运行
 tail -f /dev/null
