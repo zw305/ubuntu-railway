4a4bbbd093609bb7f4fe6ac2d67a5c2ba08333d7
zw305 <zw11305@outlook.com>
2026-07-19 11:12:42 +0800

    Configure xfce4 to disable lock screen in VNC
    Added configuration to disable screen lock and sleep for xfce4 in VNC setup.

--- start-vnc.sh
+++ start-vnc.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# 1. 启动 VNC 服务器（关闭自带的锁屏以防卡死）
+# 禁用 xfce4 的锁屏和休眠配置
+su vncuser -c "mkdir -p /home/vncuser/.config/xfce4/xfconf/xfce-perchannel-xml"
+su vncuser -c "echo '<?xml version=\"1.0\" encoding=\"UTF-8\"?><channel name=\"xfce4-power-manager\" version=\"1.0\"><property name=\"xfce4-power-manager\" type=\"empty\"><property name=\"lock-screen-suspend-hibernate\" type=\"bool\" value=\"false\"/></property></channel>' > /home/vncuser/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml"
+
+# 1. 启动 VNC 服务器
 su vncuser -c "vncserver -geometry 1280x720 -depth 24 -localhost no :1"
 
 # 2. 启动 noVNC 
