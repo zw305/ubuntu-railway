90a8b633e75d304ceae23021d01805297132890b
zw305 <zw11305@outlook.com>
2026-07-19 18:28:06 +0800

    Start VNC server by uncommenting command
    Uncomment vncserver command to start VNC server.

--- start-vnc.sh
+++ start-vnc.sh
@@ -5,7 +5,8 @@ su vncuser -c "mkdir -p /home/vncuser/.config/xfce4/xfconf/xfce-perchannel-xml"
 su vncuser -c "echo '<?xml version=\"1.0\" encoding=\"UTF-8\"?><channel name=\"xfce4-power-manager\" version=\"1.0\"><property name=\"xfce4-power-manager\" type=\"empty\"><property name=\"lock-screen-suspend-hibernate\" type=\"bool\" value=\"false\"/></property></channel>' > /home/vncuser/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml"
 
 # 1. 启动 VNC 服务器
-su vncuser -c "vncserver -geometry 1280x720 -depth 24 -localhost no :1"
+# su vncuser -c "vncserver -geometry 1280x720 -depth 24 -localhost no :1"
+vncserver -geometry 1280x720 -depth 24 -localhost no :1
 
 # 2. 启动 noVNC 
 websockify --web=/usr/share/novnc 6080 localhost:5901
